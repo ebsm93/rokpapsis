f2e1168838c917d134abf7696ab9eaef3c1bc592
ebs <ebs.m93@gmail.com>
2022-01-11 19:49:55 -0800

    complete overhaul. project complete and working.

--- rps.js
+++ rps.js
@@ -1,98 +1,127 @@
-let usr_name;
+//SET GLOBAL VARIABLES
+let playerName;
+let victoryPoints;
+let match;
 let outcome;
-let playerScore;
-let cpuScore;
-game_init();
+let playerScore = 0;
+let cpuScore = 0;
 
-function game_init() {
-  alert("let's play rock paper scissors foo");
+//SET HTML selector variables
+let form = document.getElementById('form');
+const resultBoard = document.querySelector("#resultBoard");
+const resultingMoves = document.querySelector('#resultingMoves');
+const result = document.querySelector("#result");
+const final = document.querySelector("#final");
+const setMatchButton = document.querySelector('#setMatch');
+const startButton = document.querySelector('#start');
+const moveButtons = document.querySelector('.moves');
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+const nextRoundButton = document.querySelector('#nextRound');
+const resetButton = document.querySelector("#reset");
 
-  usr_name = prompt("what's your name esse?");
-  let victory_points = parseInt(prompt(`okay, ${usr_name} how many\nwins do you wanna play to?`));
-  alert(`alright ${usr_name}, it's you\nvs CPU, first to ${victory_points} wins\nwins the game.`);
+//SET BUTTON EVENT LISTENERS
+setMatchButton.addEventListener('click', () => {
+  playerName = document.querySelector('#textBox').value;
+  victoryPoints = parseInt(document.querySelector('#textBox2').value);
+  score = document.querySelector('#score');
+  score.innerHTML = `<u>score</u> <br>${playerName}: ${playerScore}<br>CPU: ${cpuScore}`;
+  match = document.querySelector('#matchDisplay');
+  match.innerHTML = `${playerName} vs CPU - First to ${victoryPoints} wins`;
+  form.style.display = "none";
+  startButton.style.display = '';
+});
 
-  gameEngine(victory_points);
+startButton.addEventListener('click', gameInit)
+
+scissorsButton.addEventListener('click', function(){play('scissors')});
+paperButton.addEventListener('click', function(){play('paper')});
+rockButton.addEventListener('click', function(){play('rock')});
+resetButton.addEventListener('click', resetGame);
+
+//SET FUNCTIONS
+function gameInit() {
+  startButton.style.display = 'none';  
+  moveButtons.style.display = "";
+  resetButton.style.display = "";
 }
 
-function gameEngine(victory_points) {
-  playerScore = 0;
-  cpuScore = 0;
-  let no_victor = true
-  while (no_victor) {
-    outcome = playRound(cpuMove(),playerMove())
-    score(outcome);
-    if (cpuScore === victory_points||playerScore === victory_points) {
-    	no_victor = false;
-    }
+function play(move) {
+  let cpu = cpuMove();
+  let player = move;
+
+  if (cpu === player){
+    outcome = 'tie';
+  } else if (cpu === 'rock' && player === 'scissors'
+     ||cpu === 'paper' && player === 'rock'
+     ||cpu === 'scissors' && player === 'paper') {
+     outcome = 'CPU wins.';
+  } else if (player === 'rock' && cpu === 'scissors'
+     ||player === 'paper' && cpu === 'rock'
+     ||player === 'scissors' && cpu === 'paper') {
+     outcome = `${playerName} wins.`;
   }
-  gameOver(playerScore,cpuScore);
+  resultingMoves.innerHTML = `${playerName} chose ${move},<br> the CPU chose ${cpu}.`
+  UpdateScore(outcome);
+  nextRoundButton.addEventListener('click', nextRound);
 }
-
 
 function cpuMove() {
-	let moveS = ['rock','paper','scissors'];
-	let value = Math.floor(Math.random()*3);
-	return moveS[value];
+  let moveS = ['rock','paper','scissors'];
+  let value = Math.floor(Math.random()*3);
+  return moveS[value];
 }
 
-function playerMove() {
-	let move = prompt("type 'rock'/'r','paper'/'p' or 'scissors'/'s' \n to play your move");
-	return pMoveAssess(move);
-}
-
-function pMoveAssess(move) {
-	if (move === 'rock'||'r') {
-		move = 'rock';
-		return move;
-	} else if (move === 'paper'||'p') {
-		move = 'paper';
-		return move;
-	} else if (move === 'scissors'||'s') {
-		move = 'scissors';
-		return move;
-	} else {
-		alert(`${move} is not a valid option\nplease try again...`);
-		playerMove();
-	}
-}
-
-function playRound(cpu,player) {
-  if (cpu === player){
-  	result = 'tie';
-  } else if (cpu === 'rock' && player === 'scissors'
-  	  ||cpu === 'paper' && player === 'rock'
-  	  ||cpu === 'scissors' && player === 'paper') {
-  	  result = 'cpu wins.';
-  } else if (player === 'rock' && cpu === 'scissors'
-  	  ||player === 'paper' && cpu === 'rock'
-  	  ||player === 'scissors' && cpu === 'paper') {
-  	  result = `${usr_name} wins.`;
-  alert(`you chose ${player}, the cpu chose ${cpu}`)
-  alert(result);
-  return result;
-  }
-}
-
-function score(outcome) {
-  if (outcome === `${usr_name} wins.`) {
+function UpdateScore() {
+  if (outcome === `${playerName} wins.`) {
     playerScore += 1;
-  } else if (outcome === 'cpu wins.') {
-  	cpuScore += 1;
+  } else if (outcome === 'CPU wins.') {
+    cpuScore += 1;
   } 
-  alert(`${usr_name} score: ${playerScore}\ncpu score: ${cpuScore}`)
+  score.innerHTML = `<u>score</u> <br>${playerName}: ${playerScore}<br>CPU: ${cpuScore}`;
+  moveButtons.style.display = "none";
+  result.innerHTML = outcome;
+  resultBoard.style.display = '';
+  checkVictory();
+  
 }
 
-function gameOver(playerScore,cpuScore) {
-  if (playerScore > cpuScore){
-	alert(`VICTORY FOR ${usr_name}`);
-  } else {
-    alert("CPU WINS.\nYOU LOSE...");
-  }
+function nextRound() {
+  resultBoard.style.display = "none";
+  moveButtons.style.display = "";
+}
 
-  let reset = confirm('play again?');
-  if (reset) {
-  	game_init();
-  } else {
-  	alert(`until next time ${usr_name}...`)
+function checkVictory() {
+  if (cpuScore === victoryPoints||playerScore === victoryPoints) {
+    gameOver();
   }
+}
+
+function gameOver() {
+  if (playerScore > cpuScore){
+    final.innerHTML = `${playerName} IS VICTORIOUS!`;
+  } else {
+    final.innerHTML = `THE CPU HAS DEFEATED YOU ...`;
+    final.style.color = 'red';
+  }
+  final.style.display = '';
+  nextRoundButton.style.display = 'none';
+}
+
+function resetGame() {
+  nextRoundButton.style.display = '';
+  moveButtons.style.display = "none";
+  resetButton.style.display = "none";
+  resultBoard.style.display = "none";
+  final.style.display = 'none';
+  resultBoard.style.display = 'none';
+  final.style.display = 'none';
+  score.innerHTML = '';
+  match.innerHTML = '';
+  form.style.display = '';
+  playerScore = 0;
+  cpuScore = 0;
+  document.querySelector('#textBox').value = '';
+  document.querySelector('#textBox2').value = '';
 }
